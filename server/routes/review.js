2fe65bce043bab1698f564afec18fd8e170e5026
paalandreeas <pamorho@gmail.com>
2020-11-04 13:13:07 +0100

    Refined review post method. Refined user register method.

--- server/routes/review.js
+++ server/routes/review.js
@@ -3,6 +3,7 @@ const Movie = require("../models/movie");
 const router = express.Router();
 const Review = require("../models/review");
 const User = require("../models/user");
+const passport = require("passport");
 
 router.get("/id/:id", (req, res, next) => {
   Review.find({ _id: req.params.id })
@@ -10,27 +11,42 @@ router.get("/id/:id", (req, res, next) => {
     .catch(next);
 });
 
-router.post("/", (req, res, next) => {
-  if (req.body) {
-    Review.create(req.body)
-      .then((review) => {
-        Movie.findOneAndUpdate(
-          { _id: review.movieID },
-          { $push: { reviews: review._id } },
-          { new: true, useFindAndModify: false }
-        ).then();
-        User.findOneAndUpdate(
-          { _id: review.userID },
-          { $push: { reviews: review._id } },
-          { new: true, useFindAndModify: false }
-        ).then();
-        return review;
-      })
-      .then((review) => res.json(review))
-      .catch(next);
-  } else {
-    res.json({ error: "Not a valid review" });
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res, next) => {
+    if (req.user._id.toString() === req.body.userID) {
+      if (req.body) {
+        let reviewExists = await Review.exists(
+          { movieID: req.body.movieID } && { userID: req.body.userID }
+        );
+        if (!reviewExists) {
+          Review.create(req.body)
+            .then((review) => {
+              Movie.findOneAndUpdate(
+                { _id: review.movieID },
+                { $push: { reviews: review._id } },
+                { new: true, useFindAndModify: false }
+              ).then();
+              User.findOneAndUpdate(
+                { _id: review.userID },
+                { $push: { reviews: review._id } },
+                { new: true, useFindAndModify: false }
+              ).then();
+              return review;
+            })
+            .then((review) => res.json(review))
+            .catch(next);
+        } else {
+          res.json({ error: "You have already reviewed this movie" });
+        }
+      } else {
+        res.json({ error: "Not a valid review" });
+      }
+    } else {
+      res.json({ error: "That is not your user" });
+    }
   }
-});
+);
 
 module.exports = router;
