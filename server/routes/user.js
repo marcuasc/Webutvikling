434fc13719cae767dfb79412c553383894b89b3d
Oscar <ombergan@stud.ntnu.no>
2020-11-06 10:25:36 +0100

    Fixed token return on register

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,70 +4,61 @@ const User = require("../models/user");
 const passport = require("passport");
 const utils = require("../middleware/utils");
 
-router.get(
-  "/protected",
-  passport.authenticate("jwt", { session: false }),
-  (req, res, next) => {
-    res.status(200).json({
-      success: true,
-      msg: "You are succesfully authenticated to this route!",
-    });
-  }
-);
-
 router.post("/login", function (req, res, next) {
-  User.findOne({ username: req.body.username })
-    .then((user) => {
-      if (!user) {
-        res.status(401).json({ sucess: false, msg: "Could not find user" });
-      }
+    User.findOne({username: req.body.username})
+        .then((user) => {
+            if (!user) {
+                res.status(401).json({sucess: false, msg: "Could not find user"});
+            }
 
-      const isValid = utils.validPassword(
-        req.body.password,
-        user.hash,
-        user.salt
-      );
+            const isValid = utils.validPassword(
+                req.body.password,
+                user.hash,
+                user.salt
+            );
 
-      if (isValid) {
-        const tokenObject = utils.issueJWT(user);
-
-        res.status(200).json({
-          success: true,
-          token: tokenObject,
-          expiresIn: tokenObject.expires,
+            if (isValid) {
+                const tokenObject = utils.issueJWT(user);
+                tokenObject.success = true;
+                res.status(200).json(
+                    tokenObject
+                );
+            } else {
+                res.status(401).json({success: false, msg: "Wrong password"});
+            }
+        })
+        .catch((err) => {
+            next(err);
         });
-      } else {
-        res.status(401).json({ success: false, msg: "WRONG PASSWORD!!!!" });
-      }
-    })
-    .catch((err) => {
-      next(err);
-    });
 });
 
 router.post("/register", async (req, res, next) => {
-  if (!(await User.exists({ username: req.body.username }))) {
-    const saltHash = utils.genPassword(req.body.password);
+    if (!(await User.exists({username: req.body.username}))) {
+        const saltHash = utils.genPassword(req.body.password);
 
-    const salt = saltHash.salt;
-    const hash = saltHash.hash;
+        const salt = saltHash.salt;
+        const hash = saltHash.hash;
 
-    const newUser = new User({
-      username: req.body.username,
-      hash: hash,
-      salt: salt,
-    });
+        const newUser = new User({
+            username: req.body.username,
+            hash: hash,
+            salt: salt,
+        });
 
-    try {
-      newUser.save().then((user) => {
-        res.json({ success: true, user: user });
-      });
-    } catch (err) {
-      res.json({ success: false, msg: err });
+        try {
+            newUser.save().then((user) => {
+                const tokenObject = utils.issueJWT(user);
+                tokenObject.success = true;
+                res.status(200).json(
+                    tokenObject
+                );
+            });
+        } catch (err) {
+            res.json({success: false, msg: err});
+        }
+    } else {
+        res.json({error: "Username already taken"});
     }
-  } else {
-    res.json({ error: "Username already taken" });
-  }
 });
 
 module.exports = router;
